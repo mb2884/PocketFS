d01efe7f729380198aed7c5d0df5c9fd9c903ec9
mb2884 <mb2884@princeton.edu>
2024-03-07 06:28:08 -0500

    files and dirs working

--- source/main.c
+++ source/main.c
@@ -11,7 +11,7 @@
 #include "test.h"
 #include "display.h"
 
-#define MAX_DIRECTORIES 16
+#define MAX_ITEMS 16
 
 void setup()
 {
@@ -29,8 +29,8 @@ void setup()
   BG_PALETTE[0] = RGB5(4, 4, 5);
 }
 
-// Function to print all subdirectories and allow selection
-Directory *printCursor(Directory *directory, int cursorPosition)
+// Function to print all subdirectories and files and allow selection
+Directory *printCursor(Directory *directory, int cursorPosition, int *selectedIsFile)
 {
   if (!directory)
   {
@@ -38,18 +38,41 @@ Directory *printCursor(Directory *directory, int cursorPosition)
     return NULL;
   }
 
-  printf("Subdirectories: \n");
+  printf("Contents of directory '%s':\n", directory->name);
+
+  // Calculate total number of items
+  int totalItems = directory->num_subdirectories + directory->num_files;
+
+  // Print subdirectories
+  printf("Subdirectories:\n");
   for (int i = 0; i < directory->num_subdirectories; ++i)
   {
     if (i == cursorPosition)
     {
       printf("-> ");
+      *selectedIsFile = 0; // Selected entry is a directory
     }
     else
     {
       printf("   ");
     }
     printf("%s\n", directory->subdirectories[i]->name);
+  }
+
+  // Print files
+  printf("Files:\n");
+  for (int i = 0; i < directory->num_files; ++i)
+  {
+    if ((directory->num_subdirectories + i) == cursorPosition)
+    {
+      printf("-> ");
+      *selectedIsFile = 1; // Selected entry is a file
+    }
+    else
+    {
+      printf("   ");
+    }
+    printf("%s\n", directory->files[i]->name);
   }
 
   return directory;
@@ -74,6 +97,10 @@ int main(void)
   Directory *subDir4 = createDirectory("dir4", rootDir);
   Directory *subDir5 = createDirectory("dir5", rootDir);
 
+  File *file1 = createFile("file1.txt", rootDir);
+  File *file2 = createFile("file2.txt", rootDir);
+  File *file3 = createFile("file3.txt", rootDir);
+
   int cursorPosition = 0;
   Directory *currentDirectory = rootDir;
 
@@ -81,7 +108,11 @@ int main(void)
   {
     clearScr();
 
-    currentDirectory = printCursor(currentDirectory, cursorPosition);
+    // Flag to determine if the selected item is a file (1) or directory (0)
+    int selectedIsFile = 0;
+
+    // Print directory contents and allow selection
+    currentDirectory = printCursor(currentDirectory, cursorPosition, &selectedIsFile);
 
     // Get current values for keys
     scanKeys();
@@ -91,11 +122,11 @@ int main(void)
 
     if (keys_pressed & KEY_DOWN)
     {
-      cursorPosition = (cursorPosition + 1) % currentDirectory->num_subdirectories;
+      cursorPosition = (cursorPosition + 1) % (currentDirectory->num_subdirectories + currentDirectory->num_files);
     }
     else if (keys_pressed & KEY_UP)
     {
-      cursorPosition = (cursorPosition - 1 + currentDirectory->num_subdirectories) % currentDirectory->num_subdirectories;
+      cursorPosition = (cursorPosition - 1 + (currentDirectory->num_subdirectories + currentDirectory->num_files)) % (currentDirectory->num_subdirectories + currentDirectory->num_files);
     }
     else if (keys_pressed & KEY_LEFT)
     {
@@ -109,7 +140,12 @@ int main(void)
     else if (keys_pressed & KEY_RIGHT)
     {
       // Check if a subdirectory is highlighted
-      if (cursorPosition >= 0 && cursorPosition < currentDirectory->num_subdirectories)
+      if (selectedIsFile)
+      {
+        // Handle file selection (navigate to edit file screen)
+        printf("Selected file: %s\n", currentDirectory->files[cursorPosition - currentDirectory->num_subdirectories]->name);
+      }
+      else if (cursorPosition >= 0 && cursorPosition < currentDirectory->num_subdirectories)
       {
         // Set the highlighted subdirectory as the new root directory
         currentDirectory = currentDirectory->subdirectories[cursorPosition];
@@ -118,27 +154,38 @@ int main(void)
     }
     else if (keys_pressed & KEY_B)
     {
-      if (currentDirectory->num_subdirectories < MAX_DIRECTORIES)
+      // Create a new directory within the current directory
+      if (currentDirectory->num_subdirectories < MAX_ITEMS)
       {
-        Directory *subDir = createDirectory("dir", currentDirectory);
+        Directory *newDir = createDirectory("new_dir", currentDirectory);
+      }
+    }
+    else if (keys_pressed & KEY_SELECT)
+    {
+      // Create a new file within the current directory
+      if ((currentDirectory->num_subdirectories + currentDirectory->num_files) < MAX_ITEMS)
+      {
+        File *newFile = createFile("new_file.txt", currentDirectory);
       }
     }
     else if (keys_pressed & KEY_A)
     {
-      // Check if a subdirectory is highlighted
+      // Check if a subdirectory or file is highlighted
       if (cursorPosition >= 0 && cursorPosition < currentDirectory->num_subdirectories)
       {
         // Delete the highlighted subdirectory
         Directory *dirToDelete = currentDirectory->subdirectories[cursorPosition];
-        Directory *parentDirectory = dirToDelete->parentDirectory;
         deleteDirectory(dirToDelete);
 
         // Set the current directory to the parent directory of the deleted one
-        if (parentDirectory != NULL)
-        {
-          currentDirectory = parentDirectory;
-          cursorPosition = 0; // Reset cursor position to the first subdirectory
-        }
+        currentDirectory = dirToDelete->parentDirectory;
+        cursorPosition--; // Reset cursor position to the first subdirectory
+      }
+      else if (cursorPosition >= currentDirectory->num_subdirectories && cursorPosition < (currentDirectory->num_subdirectories + currentDirectory->num_files))
+      {
+        // Delete the highlighted file
+        File *fileToDelete = currentDirectory->files[cursorPosition - currentDirectory->num_subdirectories];
+        deleteFile(fileToDelete);
       }
     }
 
